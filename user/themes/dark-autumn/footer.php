f6cc696d01c786372aadeadd69fa79cd4379e5e8
heptat <heptat@gmail.com>
2012-10-23 12:26:08 +0000

    add theme

---
+++ user/themes/dark-autumn/footer.php
@@ -0,0 +1,11 @@
+				<div id="footer" class="clear">
+					<p>The site is proudly powered by <a href="http://habariproject.org" rel="external">Habari</a>.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php $theme->footer(); ?>
+	
+</body>
+</html>
